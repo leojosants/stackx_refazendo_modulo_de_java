73558fc58c5b918a836e74cfbbb1bee86e7c2b06
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-17 19:18:47 -0300

    update file

--- 05_tarefas_estrutura_de_controle/01_for/03_estatistica/src/App.java
+++ 05_tarefas_estrutura_de_controle/01_for/03_estatistica/src/App.java
@@ -4,60 +4,61 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner leitor = new Scanner(System.in);
 
-        int codigo;
-        int numeroVeiculos;
-        int numeroAcidentes;
-        int maiorIndiceAcidente = 0;
-        int menorIndiceAcidente = 100000000;
-        int codigoCidadeMaiorIndiceAcidente = 0;
-        int codigoCidadeMenorIndiceAcidente = 0;
-        int totalVeiculos = 0;
-        int totalAcidentesCidadesMenos2000 = 0;
-        int contadorCidadesMenos2000 = 0;
-        double mediaTodosVeiculos = 0.0;
-        double mediaAcidentesCidadesMenos2000 = 0.0;
+        int code;
+        int numberVehicles;
+        int numberAccidents;
+        int biggestIndexAccident = 0;
+        int minorIndexAccident = 100000000;
+        int codeMajorCityIndexAccident = 0;
+        int CodeCityMenorIndexAccident = 0;
+        int totalVehicles = 0;
+        int totalAccidentsCitiesLess2000Vehicles = 0;
+        int counterCitiesLess2000Vehicles = 0;
+        double mediaAllVehicles = 0.0;
+        double mediaAccidentsCitiesLess2000Vehicles = 0.0;
 
         for (int i = 1; i <= 5; i++) {
             System.out.printf("\nDADOS DA %dª CIDADE%n", i);
             System.out.print("Código da cidade: ");
-            codigo = leitor.nextInt();
+            code = leitor.nextInt();
 
             System.out.print("Número de veículos de passeio: ");
-            numeroVeiculos = leitor.nextInt();
+            numberVehicles = leitor.nextInt();
 
             System.out.print("Número de acidentes de trânsito com vítimas: ");
-            numeroAcidentes = leitor.nextInt();
+            numberAccidents = leitor.nextInt();
 
-            totalVeiculos += numeroVeiculos;
+            totalVehicles += numberVehicles;
 
-            if (numeroAcidentes > maiorIndiceAcidente) {
-                maiorIndiceAcidente = numeroAcidentes;
-                codigoCidadeMaiorIndiceAcidente = codigo;
+            if (numberAccidents > biggestIndexAccident) {
+                biggestIndexAccident = numberAccidents;
+                codeMajorCityIndexAccident = code;
             }
 
-            if (numeroAcidentes < menorIndiceAcidente) {
-                menorIndiceAcidente = numeroAcidentes;
-                codigoCidadeMenorIndiceAcidente = codigo;
+            if (numberAccidents < minorIndexAccident) {
+                minorIndexAccident = numberAccidents;
+                CodeCityMenorIndexAccident = code;
             }
 
-            if (numeroAcidentes < 2_000) {
-                totalAcidentesCidadesMenos2000 += numeroAcidentes;
-                contadorCidadesMenos2000++;
-                mediaAcidentesCidadesMenos2000 = totalAcidentesCidadesMenos2000 / contadorCidadesMenos2000;
+            if (numberAccidents < 2_000) {
+                totalAccidentsCitiesLess2000Vehicles += numberAccidents;
+                counterCitiesLess2000Vehicles++;
+                mediaAccidentsCitiesLess2000Vehicles = totalAccidentsCitiesLess2000Vehicles
+                        / counterCitiesLess2000Vehicles;
             }
         }
 
-        mediaTodosVeiculos = totalVeiculos / 5;
+        mediaAllVehicles = totalVehicles / 5;
 
         System.out.printf("\nMaior índice de acidentes de trânsito é %d e pertence a cidade código %d%n",
-                maiorIndiceAcidente, codigoCidadeMaiorIndiceAcidente);
+                biggestIndexAccident, codeMajorCityIndexAccident);
 
         System.out.printf("Menor índice de acidentes de trânsito é %d e pertence a cidade código %d%n",
-                menorIndiceAcidente, codigoCidadeMenorIndiceAcidente);
+                minorIndexAccident, CodeCityMenorIndexAccident);
 
-        System.out.printf("Média de veículos nas cinco cidades juntas é: %.2f%n", mediaTodosVeiculos);
+        System.out.printf("Média de veículos nas cinco cidades juntas é: %.2f%n", mediaAllVehicles);
         System.out.printf("Média de acidentes de trânsito nas cidades com menos de 2.000 veículos de passeio é: %.2f%n",
-                mediaAcidentesCidadesMenos2000);
+                mediaAccidentsCitiesLess2000Vehicles);
 
         leitor.close();
     }
---
+++ 05_tarefas_estrutura_de_controle/02_swich_case/01_preco_de_produtos/README.md
@@ -0,0 +1,41 @@
+1. Estruturas condicionais e de repetição: Preço de produtos (Swich Case)
+
+    - Faça um programa que receba:
+        - Preço;
+
+        - Categoria:
+            - 1: limpeza; 
+            - 2: alimentação; 
+            - 3: vestuário;
+        
+        - Situação:
+            - R: produtos que necessitam de refrigeração;
+            - N: produtos que não necessitam de refrigeração
+            
+    - Calcule e mostre:
+        - O valor do aumento, usando as regras que se seguem.
+            
+            Preço       Categoria   Percentual de aumento
+            <= 25       1           5%
+                        2           8%
+                        3           10%
+
+            > 25        1           12%
+                        2           15%
+                        3           18%
+
+        - O valor do imposto, usando as regras a seguir.
+            - O produto que preencher pelo menos um dos seguintes requisitos pagará imposto equivalente a 5% do preço; caso contrário, pagará 8%. 
+            
+            - Os requisitos são:
+                - Categoria: 2
+                - Situação: R
+
+        - O novo preço, ou seja, o preço mais aumento menos imposto.
+
+        - A classificação, usando as regras a seguir.
+
+            Novo preço                  Classificação
+            <=R$50,00                   Barato
+            Entre R$50,00 e R$120,00    Normal
+            >= R4120,00                 Caro

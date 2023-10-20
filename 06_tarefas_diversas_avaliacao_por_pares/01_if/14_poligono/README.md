f0160ac58fabf6d4b44cc623ee96474c6e7165e9
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-20 19:35:49 -0300

    14. polygon (IF)

---
+++ 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/README.md
@@ -0,0 +1,15 @@
+14. Polígono (IF)
+
+    - Escreva um programa para ler o número de lados de um polígono regular e a medida do lado (em cm).
+
+    - Calcular e imprimir o seguinte:
+        - Se o número de lados for igual a 3;
+            - Escrever TRIÂNGULO e o valor da área
+
+        - Se o número de lados for igual a 4;
+            - Escrever QUADRADO e o valor da sua área.
+
+        - Se o número de lados for igual a 5;
+            - Escrever PENTÁGONO e o valor da sua área.
+
+    - Obs: O foco aqui é a lógica com o comando de controle IF, demais conhecimentos envolvidos pesquise mais. 
---
+++ 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/src/App.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner readerScanner = new Scanner(System.in);
+
+        String typeOfPolygon = "";
+        double calculationArea = 0.0;
+        double measurementSides;
+        int totalSides;
+
+        System.out.println("\nEntre com os dados solicitados abaixo de um poligono retangulo");
+
+        do {
+            System.out.print("Digite o numero de lados (número positivo): ");
+            totalSides = readerScanner.nextInt();
+        } while (totalSides < 3);
+
+        do {
+            System.out.print("Medida do lado [cm]: ");
+            measurementSides = readerScanner.nextDouble();
+        } while (measurementSides <= 0);
+
+        if (totalSides == 3) {
+            typeOfPolygon = "Triangulo";
+            calculationArea = (measurementSides * measurementSides) / 2;
+
+        } else if (totalSides == 4) {
+            typeOfPolygon = "Quadrado";
+            calculationArea = Math.pow(measurementSides, 2);
+
+        } else if (totalSides == 5) {
+            typeOfPolygon = "Pentágono";
+            calculationArea = (5 * (Math.pow(measurementSides, 2)) / (4 * (Math.tan(Math.toRadians(36)))));
+
+        }
+
+        System.out.println("\nExibindo dados");
+        System.out.printf("Total de lados: %d%n", totalSides);
+        System.out.printf("Polígono do tipo: %s%n", typeOfPolygon);
+        System.out.printf("Valor da área: %.2fcm²", calculationArea);
+
+        readerScanner.close();
+    }
+}

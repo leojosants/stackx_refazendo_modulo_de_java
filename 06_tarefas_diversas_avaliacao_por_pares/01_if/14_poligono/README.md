443172f0d001a7d24ee097fc532c9431922983a7
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-20 19:50:06 -0300

    15. complement to task 14 polygon (IF)

--- 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/README.md
+++ 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/README.md
@@ -3,6 +3,9 @@
     - Escreva um programa para ler o número de lados de um polígono regular e a medida do lado (em cm).
 
     - Calcular e imprimir o seguinte:
+        - Se o número de lados seja inferior a 3;
+            - Escrever NÃO É UM POLÍGONO.
+
         - Se o número de lados for igual a 3;
             - Escrever TRIÂNGULO e o valor da área
 
@@ -11,5 +14,8 @@
 
         - Se o número de lados for igual a 5;
             - Escrever PENTÁGONO e o valor da sua área.
+
+        - Se o número de lados seja superior a 5;
+            - Escrever POLÍGONO NÃO IDENTIFICADO.
 
     - Obs: O foco aqui é a lógica com o comando de controle IF, demais conhecimentos envolvidos pesquise mais. 
--- 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/src/App.java
+++ 06_tarefas_diversas_avaliacao_por_pares/01_if/14_poligono/src/App.java
@@ -12,17 +12,20 @@ public class App {
 
         System.out.println("\nEntre com os dados solicitados abaixo de um poligono retangulo");
 
-        do {
-            System.out.print("Digite o numero de lados (número positivo): ");
-            totalSides = readerScanner.nextInt();
-        } while (totalSides < 3);
+        // do {
+        System.out.print("Digite o numero de lados (número positivo): ");
+        totalSides = readerScanner.nextInt();
+        // } while (totalSides < 3);
 
         do {
             System.out.print("Medida do lado [cm]: ");
             measurementSides = readerScanner.nextDouble();
         } while (measurementSides <= 0);
 
-        if (totalSides == 3) {
+        if (totalSides < 3) {
+            typeOfPolygon = "Não é um polígono";
+
+        } else if (totalSides == 3) {
             typeOfPolygon = "Triangulo";
             calculationArea = (measurementSides * measurementSides) / 2;
 
@@ -34,12 +37,17 @@ public class App {
             typeOfPolygon = "Pentágono";
             calculationArea = (5 * (Math.pow(measurementSides, 2)) / (4 * (Math.tan(Math.toRadians(36)))));
 
+        } else if (totalSides > 5) {
+            typeOfPolygon = "Polígono não identificado";
         }
 
         System.out.println("\nExibindo dados");
         System.out.printf("Total de lados: %d%n", totalSides);
         System.out.printf("Polígono do tipo: %s%n", typeOfPolygon);
-        System.out.printf("Valor da área: %.2fcm²", calculationArea);
+
+        if ((totalSides > 3) && (totalSides < 5)) {
+            System.out.printf("Valor da área: %.2fcm²", calculationArea);
+        }
 
         readerScanner.close();
     }

8627c437d453cfe070dbd5ed417863ee6f82447d
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-21 14:38:18 -0300

    implemented exercise

--- 06_tarefas_diversas_avaliacao_por_pares/01_if/19_identificar_erros/src/App.java
+++ 06_tarefas_diversas_avaliacao_por_pares/01_if/19_identificar_erros/src/App.java
@@ -1,5 +1,38 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // a
+        /*
+         * int age = 65;
+         * 
+         * if (age >= 65) {
+         *      System.out.println("melhor idade");
+         * }
+         */
+
+        // b
+        /*
+         * int gender = 0;
+         * 
+         * if (gender == 1) {
+         *      System.out.println("Masculino");
+         * 
+         * } else if (gender == 2) {
+         *      System.out.println("Feminino");
+         * }
+         */
+
+        //  c
+        
+        double price = 10.51;
+
+        if (price > 10.50) {
+            price = price * 1.2;
+
+        } else {
+            price = price * 1.35;
+        }
+        
+        System.out.println(price);
     }
 }

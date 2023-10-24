eec24081874dbfc1da0ebae26a1b8d98d502ec7b
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-24 16:46:16 -0300

    update file

--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_01/src/App.java
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_01/src/App.java
@@ -2,12 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        DisplaysData(numbers);
 
-        System.out.println("\nExibindo elementos do vetor\n");
+    }
+
+    /* SUB-ROUTINES */
+    public static void DisplaysData(int numbers[]) {
+        System.out.println("\nExibindo dados");
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.printf("Posição %d: %d%n", i, numbers[i]);
         }
-
     }
 }

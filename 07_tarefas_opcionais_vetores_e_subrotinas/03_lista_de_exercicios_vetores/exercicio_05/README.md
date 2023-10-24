9cd1e5b98d07c7164b97888acd944417e56d19c7
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-24 19:43:06 -0300

    exercise 05

--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_05/README.md
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_05/README.md
@@ -1,18 +1,7 @@
-## Getting Started
+# Exercício 05
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+    - Dado um vetor V = {91, 10, 50, 89, 45, 80, 2, 45, 3, 105, 95, 13, 26, 49, 50};
+    
+    - Criar um programa que receba um número pelo teclado;
+    
+    - Verifique e imprima na tela se este número existe no vetor.
--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_05/src/App.java
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_05/src/App.java
@@ -1,12 +1,76 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        DisplaysData();
+        Scanner scanner = new Scanner(System.in);
 
+        try {
+            int numbers[] = { 91, 10, 50, 89, 45, 80, 2, 45, 3, 105, 95, 13, 26, 49, 50 };
+            int copyNumbers[] = ReturnsCopyNumbers(numbers);
+            int numberReceived = RequestsData(scanner);
+            boolean numberExistsInVector = CheckIfNumberExistsInVector(numberReceived, copyNumbers);
+            String numberExistsInVectorString = ConvertBooleanToString(numberExistsInVector);
+            DisplaysData(numberReceived, numberExistsInVectorString);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Erro, entrada inválida de dados");
+
+        } catch (Exception e) {
+            System.out.println(e);
+
+        } finally {
+            scanner.close();
+            System.out.println("Fim do programa");
+        }
     }
 
     /* SUB-ROUTINES */
-    public static void DisplaysData() {
+    public static int RequestsData(Scanner scanner) {
+        int number = 0;
+
+        do {
+            System.out.print("Digite um número inteiro [positivo]: ");
+            number = scanner.nextInt();
+        } while (number < 0);
+
+        return number;
+    }
+
+    public static int[] ReturnsCopyNumbers(int numbers[]) {
+        int copy[] = new int[numbers.length];
+        System.arraycopy(numbers, 0, copy, 0, numbers.length);
+        return copy;
+    }
+
+    public static boolean CheckIfNumberExistsInVector(int numberReceived, int copyNumbers[]) {
+        boolean numberExistsInVector = false;
+
+        for (int i = 0; i < copyNumbers.length; i++) {
+            if (copyNumbers[i] == numberReceived) {
+                numberExistsInVector = true;
+            }
+        }
+
+        return numberExistsInVector;
+    }
+
+    public static String ConvertBooleanToString(boolean numberExistsInVector) {
+        String responseConvertedToString = "";
+
+        if (numberExistsInVector == true) {
+            responseConvertedToString="SIM";
+        }else {
+            responseConvertedToString="NAO";
+        }
+
+        return responseConvertedToString.toUpperCase();
+    }
+
+    public static void DisplaysData(int numberReceived, String numberExistsInVectorString) {
         System.out.println("\nExibindo dados");
+        System.out.printf("Número recebido: %d%n", numberReceived);
+        System.out.printf("Número recebido existe no vetor: %s%n", numberExistsInVectorString);
     }
 }

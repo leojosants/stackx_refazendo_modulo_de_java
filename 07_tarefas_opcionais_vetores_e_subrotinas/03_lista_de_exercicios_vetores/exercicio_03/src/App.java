076138a44425c5d19749660279ef7f15e634d784
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-24 18:38:10 -0300

    update file

--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_03/src/App.java
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_03/src/App.java
@@ -2,25 +2,33 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         double numbers[] = { 85.0, 105.0, 40.0, 90.0, 35.0, 65.0, 33.0, 22.0, 19.0, 50.0 };
-        double sumNumbers = ReturnsSumNumbers(numbers);
-        double average = ReturnsAverage(numbers, sumNumbers);
+        double copyNumbers[] = ReturnsCopyNumbers(numbers);
+
+        double sumNumbers = ReturnsSumNumbers(copyNumbers);
+        double average = ReturnsAverage(copyNumbers, sumNumbers);
         DisplaysData(average);
     }
 
     /* SUB-ROUTINES */
-    public static double ReturnsSumNumbers(double numbers[]) {
+    public static double[] ReturnsCopyNumbers(double numbers[]) {
+        double copy[] = new double[numbers.length];
+        System.arraycopy(numbers, 0, copy, 0, numbers.length);
+        return copy;
+    }
+
+    public static double ReturnsSumNumbers(double copyNumbers[]) {
         double sumNumbers = 0.0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            sumNumbers += numbers[i];
+        for (int i = 0; i < copyNumbers.length; i++) {
+            sumNumbers += copyNumbers[i];
         }
 
         return sumNumbers;
     }
 
-    public static double ReturnsAverage(double numbers[],double sumNumbers) {
+    public static double ReturnsAverage(double copyNumbers[], double sumNumbers) {
         double average = 0.0;
-        int totalNumeros = numbers.length;
+        int totalNumeros = copyNumbers.length;
         average = sumNumbers / totalNumeros;
         return average;
     }
@@ -30,3 +38,36 @@ public class App {
         System.out.printf("Média: %.2f%n", average);
     }
 }
+// public class App {
+// public static void main(String[] args) throws Exception {
+
+// double numbers[] = { 85.0, 105.0, 40.0, 90.0, 35.0, 65.0, 33.0, 22.0, 19.0,
+// 50.0 };
+// double sumNumbers = ReturnsSumNumbers(numbers);
+// double average = ReturnsAverage(numbers, sumNumbers);
+// DisplaysData(average);
+// }
+
+// /* SUB-ROUTINES */
+// public static double ReturnsSumNumbers(double numbers[]) {
+// double sumNumbers = 0.0;
+
+// for (int i = 0; i < numbers.length; i++) {
+// sumNumbers += numbers[i];
+// }
+
+// return sumNumbers;
+// }
+
+// public static double ReturnsAverage(double numbers[],double sumNumbers) {
+// double average = 0.0;
+// int totalNumeros = numbers.length;
+// average = sumNumbers / totalNumeros;
+// return average;
+// }
+
+// public static void DisplaysData(double average) {
+// System.out.println("\nExibindo dados");
+// System.out.printf("Média: %.2f%n", average);
+// }
+// }

3f38bd93adaae606c0b45becb865f7b749a24810
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-24 18:43:25 -0300

    update file

--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_04/src/App.java
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_04/src/App.java
@@ -2,20 +2,26 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int copyNumbers[] = ReturnsCopyNumbers(numbers);
 
-        DisplaysData(numbers);
+        DisplaysData(copyNumbers);
     }
 
     /* SUB-ROUTINES */
-    public static void returnsSymmetricalElements(int numbers[]) {
+    public static int[] ReturnsCopyNumbers(int numbers[]) {
+        int copy[] = new int[numbers.length];
+        System.arraycopy(numbers, 0, copy, 0, numbers.length);
+        return copy;
+    }
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.printf("%d - %d%n", numbers[i], numbers[9-i]);
+    public static void returnsSymmetricalElements(int copyNumbers[]) {
+        for (int i = 0; i < copyNumbers.length; i++) {
+            System.out.printf("%d - %d%n", copyNumbers[i], copyNumbers[9 - i]);
         }
     }
 
-    public static void DisplaysData(int numbers[]) {
+    public static void DisplaysData(int copyNumbers[]) {
         System.out.println("\nExibindo dados");
-        returnsSymmetricalElements(numbers);
+        returnsSymmetricalElements(copyNumbers);
     }
 }

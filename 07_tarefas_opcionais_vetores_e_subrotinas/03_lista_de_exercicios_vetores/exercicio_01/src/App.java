d761829f427bd8d4007e34afffec93e145b5cfba
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-24 18:21:12 -0300

    update file

--- 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_01/src/App.java
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_01/src/App.java
@@ -2,16 +2,23 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        DisplaysData(numbers);
+        int copyNumbers[] = ReturnsCopyNumbers(numbers);
+        DisplaysData(copyNumbers);
 
     }
 
     /* SUB-ROUTINES */
-    public static void DisplaysData(int numbers[]) {
+    public static int[] ReturnsCopyNumbers(int numbers[]) {
+        int copy[] = new int[numbers.length];
+        System.arraycopy(numbers, 0, copy, 0, numbers.length);
+        return copy;
+    }
+
+    public static void DisplaysData(int copyNumbers[]) {
         System.out.println("\nExibindo dados");
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.printf("Posição %d: %d%n", i, numbers[i]);
+        for (int i = 0; i < copyNumbers.length; i++) {
+            System.out.printf("Posição %d: %d%n", i, copyNumbers[i]);
         }
     }
 }
---
+++ 07_tarefas_opcionais_vetores_e_subrotinas/03_lista_de_exercicios_vetores/exercicio_05/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        DisplaysData();
+
+    }
+
+    /* SUB-ROUTINES */
+    public static void DisplaysData() {
+        System.out.println("\nExibindo dados");
+    }
+}

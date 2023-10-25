ecac72aa432d43ed5ea9b6be254e1483f862fe40
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-25 20:09:22 -0300

    exercise 03

---
+++ 07_tarefas_opcionais_vetores_e_subrotinas/04_lista_de_exercicios_subrotinas/exercicio_03/README.md
@@ -0,0 +1,45 @@
+# Exercício 03
+
+    - Criar um Programa que:
+    
+        - Receba a quantidade de elementos a serem digitados;
+        
+        - Entrar com esses elementos;
+        
+        - Mostre qual é o maior e o menor número inteiro digitado em uma sequência;        
+        
+        - Exibir o maior e o menor número;
+
+
+    - Para isso deverá adotar os seguintes requisitos:
+
+        - Criar uma sub-rotina que:
+        
+            - Receba como parâmetro o vetor de inteiros;
+            
+            - Preencher cada posição utilizando entrada padrão (Scanner);
+
+
+        - Criar uma sub-rotina chamada printMaiorNumero() que:
+        
+            - Receba o vetor como parâmetro;
+            
+            - Verifica e imprima o maior número;
+
+
+        - Criar uma sub-rotina chamada printMenorNumero() que:
+        
+            - Receba o vetor como parâmetro;
+            
+            - Verifica e imprima o menor número;
+
+
+        - O método  principal (main) deverá:
+
+            - Receber via teclado, a quantidade de elementos a serem preenchidos;
+            
+            - Criar um vetor de inteiros;
+
+            - Efetuar o preenchimento deste vetor;
+
+            - Com o uso das sub-rotinas (métodos) criadas, imprimir o maior e menor número digitado.
---
+++ 07_tarefas_opcionais_vetores_e_subrotinas/04_lista_de_exercicios_subrotinas/exercicio_03/src/App.java
@@ -0,0 +1,71 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            int numberOfNumbersToBeReceived = 0;
+            int numbersReceived[];
+
+            do {
+                System.out.print(
+                        "\nDigite a quantidade de números que serão recebidos [positivo / inteiro]: ");
+                numberOfNumbersToBeReceived = scanner.nextInt();
+            } while (numberOfNumbersToBeReceived < 0);
+
+            numbersReceived = new int[numberOfNumbersToBeReceived];
+
+            FillVector(numbersReceived, scanner);
+
+            System.out.println("\nExibindo informação");
+
+            PrintLargerNumber(numbersReceived);
+            PrintSmallestNumber(numbersReceived);
+
+        } catch (InputMismatchException e) {
+            System.out.println("--> Erro: entrada de inválida de dados");
+
+        } finally {
+            scanner.close();
+            System.out.println("Fim do programa");
+        }
+    }
+
+    /* SUB-ROUTINES */
+    public static void FillVector(int vector[], Scanner scanner) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.printf("Digite o %dº número inteiro [positivo]: ", (i + 1));
+            vector[i] = scanner.nextInt();
+        }
+    }
+
+    public static void PrintLargerNumber(int vector[]) {
+        int higherNumber = 0;
+        higherNumber = vector[0];
+
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] > higherNumber) {
+                higherNumber = vector[i];
+            }
+        }
+
+        System.out.printf("Maior número digitado: %d%n", higherNumber);
+    }
+
+    public static void PrintSmallestNumber(int vector[]) {
+        int smallestNumber = 0;
+        smallestNumber = vector[0];
+
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] < smallestNumber) {
+                smallestNumber = vector[i];
+            }
+        }
+
+        System.out.printf("Menor número digitado: %d%n", smallestNumber);
+    }
+}

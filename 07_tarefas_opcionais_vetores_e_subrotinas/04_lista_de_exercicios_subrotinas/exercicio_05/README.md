e85aa0ded2c602a24366dcf4be531fe8c1143dbf
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-25 22:36:44 -0300

    exercise 05

---
+++ 07_tarefas_opcionais_vetores_e_subrotinas/04_lista_de_exercicios_subrotinas/exercicio_05/README.md
@@ -0,0 +1,31 @@
+# Exercício 05
+
+    - Criar um programa que:
+
+        - Calcule a média geral de uma turma, de acordo com os seguintes requisitos:
+
+            - Criar uma sub-rotina que:
+                -  Preencha um vetor de números decimais, passado como argumento;
+                
+                - Utilize entrada padrão (Scanner);
+
+
+            - Criar uma sub-rotina chamada calcularMediaGeral() que:
+                - Receba um vetor preenchido de números decimais;
+
+                - Processe o somatório dos valores do vetor;
+                
+                - calcule a média geral com base no somatório e na quantidade de elementos do vetor;
+
+                - Retorne o resultado do somatório;
+
+
+        - O método principal (main) deverá: 
+            
+            - Solicitar o tamanho da turma (quantidade de alunos);
+
+            - Criar um vetor de números decimais com o tamanho solicitado;
+
+            - Em seguida, deverá efetuar o preenchimento das médias no vetor criado;
+
+            - E por fim, deverá calcular a média geral e imprimir o resultado.
---
+++ 07_tarefas_opcionais_vetores_e_subrotinas/04_lista_de_exercicios_subrotinas/exercicio_05/src/App.java
@@ -0,0 +1,77 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            int numberOfStudents = RequestNumberOfStudents(scanner);
+            double averagesReceived[] = new double[numberOfStudents];
+
+            FillVectorWithReceivedAverages(averagesReceived, scanner);
+
+            double copyAveragesReceived[] = CopyVector(averagesReceived);
+            double overallAverage = CalculateOverallAverage(copyAveragesReceived);
+
+            DisplayData(overallAverage);
+
+        } catch (InputMismatchException e) {
+            System.out.println("-> Erro: entrada inválida de dados");
+
+        } finally {
+            scanner.close();
+            System.out.println("Programa finalizado");
+        }
+    }
+
+    /* SUB-ROUTINES */
+    public static int RequestNumberOfStudents(Scanner scanner) {
+        int numberOfStudents = 0;
+
+        System.out.println();
+
+        do {
+            System.out.print("Digite a quantidade de alunos [somente valor positivo / inteiro]: ");
+            numberOfStudents = scanner.nextInt();
+        } while (numberOfStudents < 0);
+
+        return numberOfStudents;
+    }
+
+    public static void FillVectorWithReceivedAverages(double vector[], Scanner scanner) {
+        System.out.printf("\nInforme a(s) %d média(s)  abaixo, utilize virgula como separador%n", vector.length);
+
+        for (int i = 0; i < vector.length; i++) {
+            do {
+                System.out.printf("Digite a %dª média [valor positivo]: ", (i + 1));
+                vector[i] = scanner.nextDouble();
+            } while (vector[i] < 0.0);
+        }
+    }
+
+    public static double[] CopyVector(double vector[]) {
+        double vectorCopied[] = new double[vector.length];
+        System.arraycopy(vector, 0, vectorCopied, 0, vector.length);
+        return vectorCopied;
+    }
+
+    public static double CalculateOverallAverage(double vector[]) {
+        double sumOfAverages = 0.0;
+        double overallAverage = 0.0;
+
+        for (int i = 0; i < vector.length; i++) {
+            sumOfAverages += vector[i];
+        }
+
+        overallAverage = sumOfAverages / vector.length;
+        return overallAverage;
+    }
+
+    public static void DisplayData(double overallAverage) {
+        System.out.println("\nExibindo dados");
+        System.out.printf("Média geral: %.2f%n", overallAverage);
+    }
+}

54b5e3ac3451bf36352a6c218add7e1ad2f9a505
Leonardo Santos <leonardojosantos@gmail.com>
2023-10-28 16:17:04 -0300

    update file

--- 09_exercicios_praticos_orientacao_a_objetos/01_empresa_com_10_funcionarios_versao_01/src/App.java
+++ 09_exercicios_praticos_orientacao_a_objetos/01_empresa_com_10_funcionarios_versao_01/src/App.java
@@ -10,32 +10,32 @@ public class App {
 
         try {
 
-            int totalDeFuncionarios = 10;
+            int totalNumberOfEmployees = 10;
 
-            Employee employee_vector[] = new Employee[totalDeFuncionarios];
+            Employee employee_vector[] = new Employee[totalNumberOfEmployees];
 
-            int contador = 0;
+            int counter = 0;
 
             System.out.println("\nEntre com os dados abaixo");
 
             do {
-                System.out.printf("\nCadastrando %dº funcionário %n", (contador + 1));
+                System.out.printf("\nCadastrando %dº funcionário %n", (counter + 1));
 
-                int codigo = solicitarCodigo(scanner);
-                int numeroDeHorasTrabalhadasNoMes = solicitarNumeroDeHorasTravalhadasNoMes(scanner);
-                char turno = solicitarTurno(scanner);
-                char categoria = solicitarCategoria(scanner);
+                int employeeCode = requestEmployeeCode(scanner);
+                int numberOfHoursWorkedInTheMonth = requestNumberOfHoursWorked(scanner);
+                char employeeWorkShift = requestEmployeeWorkShift(scanner);
+                char employeeJobCategory = requestEmployeeJobCategory(scanner);
 
-                employee_vector[contador] = new Employee(codigo, numeroDeHorasTrabalhadasNoMes, turno, categoria);
+                employee_vector[counter] = new Employee(employeeCode, numberOfHoursWorkedInTheMonth, employeeWorkShift, employeeJobCategory);
 
                 System.out.println("-> Funcionário cadastrado com sucesso");
                 System.out.println("\n=====**=====**=====Funcionário cadastrado com sucesso=====**=====**=====");
 
-                contador++;
-            } while (contador < totalDeFuncionarios);
+                counter++;
+            } while (counter < totalNumberOfEmployees);
 
             for (int i = 0; i < employee_vector.length; i++) {
-                employee_vector[i].exibirDados();
+                employee_vector[i].displayData();
             }
 
         } catch (InputMismatchException e) {
@@ -51,84 +51,84 @@ public class App {
     }
 
     /* SUB-ROUTINES */
-    public static int solicitarCodigo(Scanner scanner) {
-        int codigo = 0;
+    public static int requestEmployeeCode(Scanner scanner) {
+        int employeeCode = 0;
 
         do {
             System.out.print("Digite um valor positivo e inteiro para 'Código': ");
-            codigo = scanner.nextInt();
+            employeeCode = scanner.nextInt();
 
-            while (codigo < 0) {
+            while (employeeCode < 0) {
                 System.out.print("-> Informe [somente] valore(s) positivo(s) e inteiro(s) para 'Código': ");
-                codigo = scanner.nextInt();
+                employeeCode = scanner.nextInt();
             }
-        } while (codigo < 0);
+        } while (employeeCode < 0);
 
-        return codigo;
+        return employeeCode;
     }
 
-    public static int solicitarNumeroDeHorasTravalhadasNoMes(Scanner scanner) {
-        int numeroDeHorasTrabalhadasNoMes = 0;
+    public static int requestNumberOfHoursWorked(Scanner scanner) {
+        int numberOfHoursWorkedInTheMonth = 0;
 
         do {
             System.out
                     .print("\nDigite um valor positivo e inteiro para 'Números de horas trabalhadas no mês': ");
-            numeroDeHorasTrabalhadasNoMes = scanner.nextInt();
+            numberOfHoursWorkedInTheMonth = scanner.nextInt();
 
-            while (numeroDeHorasTrabalhadasNoMes < 0) {
+            while (numberOfHoursWorkedInTheMonth < 0) {
                 System.out.print(
                         "-> Informe [somente] valore(s) positivo(s) e inteiro(s) para 'Números de horas trabalhadas no mês': ");
-                numeroDeHorasTrabalhadasNoMes = scanner.nextInt();
+                numberOfHoursWorkedInTheMonth = scanner.nextInt();
             }
-        } while (numeroDeHorasTrabalhadasNoMes < 0);
+        } while (numberOfHoursWorkedInTheMonth < 0);
 
-        return numeroDeHorasTrabalhadasNoMes;
+        return numberOfHoursWorkedInTheMonth;
     }
 
-    public static char solicitarTurno(Scanner scanner) {
-        char turno;
+    public static char requestEmployeeWorkShift(Scanner scanner) {
+        char employeeWorkShift;
 
         do {
             System.out.println("\nEscolha uma opção abaixo para 'Turno de trabalho'");
             System.out.println("[ M - Matutino ], [ V - Vespertino ] ou [ N - Noturno ]");
 
             System.out.print("Digite sua opção: ");
-            turno = scanner.next().toUpperCase().charAt(0);
+            employeeWorkShift = scanner.next().toUpperCase().charAt(0);
 
-            while ((turno != 'M') &&
-                    (turno != 'V') &&
-                    (turno != 'N')) {
+            while ((employeeWorkShift != 'M') &&
+                    (employeeWorkShift != 'V') &&
+                    (employeeWorkShift != 'N')) {
                 System.out.println(
                         "\n-> Informe somente [ M  -  Matutino ], [ V - Vespertino ] ou [ N - Noturno ] para 'Turno de trabalho'");
 
                 System.out.print("Digite sua opção: ");
-                turno = scanner.next().toUpperCase().charAt(0);
+                employeeWorkShift = scanner.next().toUpperCase().charAt(0);
             }
-        } while ((turno != 'M') &&
-                (turno != 'V') &&
-                (turno != 'N'));
+        } while ((employeeWorkShift != 'M') &&
+                (employeeWorkShift != 'V') &&
+                (employeeWorkShift != 'N'));
 
-        return turno;
+        return employeeWorkShift;
     }
 
-    public static char solicitarCategoria(Scanner scanner) {
-        char categoria;
+    public static char requestEmployeeJobCategory(Scanner scanner) {
+        char employeeJobCategory;
 
         do {
             System.out.println("\nEscolha uma opção abaixo para 'Categoria'");
             System.out.println("[ O - Operário ] ou [ G - Gerente ]");
 
             System.out.print("Digite sua opção: ");
-            categoria = scanner.next().toUpperCase().charAt(0);
+            employeeJobCategory = scanner.next().toUpperCase().charAt(0);
 
-            while ((categoria != 'O') && (categoria != 'G')) {
+            while ((employeeJobCategory != 'O') && (employeeJobCategory != 'G')) {
                 System.out.println("\n-> Informe somente [ O - Operário ] ou [ G - Gerente] para 'Categoria'");
 
                 System.out.print("Digite sua opção: ");
-                categoria = scanner.next().toUpperCase().charAt(0);
+                employeeJobCategory = scanner.next().toUpperCase().charAt(0);
             }
-        } while ((categoria != 'O') && (categoria != 'G'));
+        } while ((employeeJobCategory != 'O') && (employeeJobCategory != 'G'));
 
-        return categoria;
+        return employeeJobCategory;
     }
 }

09f1f7e312c8bd818517142f2df2c5bf649079a8
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-07 14:14:34 -0300

    ordering a list of our objects

--- 10_conteudo_topicos_especiais/02_colecoes_java_collections/02_texto_java_collections__dominando_listas_sets_e_mapas/02_listas_de_objetos/GerenciadorDeCursos/src/TestClassList.java
+++ 10_conteudo_topicos_especiais/02_colecoes_java_collections/02_texto_java_collections__dominando_listas_sets_e_mapas/02_listas_de_objetos/GerenciadorDeCursos/src/TestClassList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import br.com.alura.classes.Classs;
 
@@ -7,7 +8,7 @@ public class TestClassList {
     public static void main(String[] args) {
 
         Classs class_1 = new Classs("Revisitando as ArrayLists", 21);
-        Classs class_2 = new Classs("Listas de objetos", 15);
+        Classs class_2 = new Classs("Listas de objetos", 20);
         Classs class_3 = new Classs("Relacionamento de listas e objetos", 15);
 
         ArrayList<Classs> classs_list = new ArrayList<Classs>();
@@ -16,8 +17,17 @@ public class TestClassList {
         classs_list.add(class_3);
 
         System.out.println(classs_list);
-        
+        System.out.println();
+
         Collections.sort(classs_list);
+        System.out.println(classs_list);
+        System.out.println();
+        
+        Collections.sort(classs_list, Comparator.comparing(Classs::getTime));
+        System.out.println(classs_list);
+        System.out.println();
+        
+        classs_list.sort(Comparator.comparing(Classs::getTime));
         System.out.println(classs_list);
     }
 }

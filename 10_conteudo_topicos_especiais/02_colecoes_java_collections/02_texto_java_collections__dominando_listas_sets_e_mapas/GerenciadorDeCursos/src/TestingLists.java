912d8b5617bbc4fd35e3279fc356e465c2b74c8f
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-07 12:11:47 -0300

    adding elements to a list

---
+++ 10_conteudo_topicos_especiais/02_colecoes_java_collections/02_texto_java_collections__dominando_listas_sets_e_mapas/GerenciadorDeCursos/src/TestingLists.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class TestingLists {
+    public static void main(String[] args) throws Exception {
+
+        String class_1 = "Conhecendo mais de listas";
+        String class_2 = "Modelando a classe Aula";
+        String class_3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> classes_list = new ArrayList<String>();
+        classes_list.add(class_1);
+        classes_list.add(class_2);
+        classes_list.add(class_3);
+
+        System.out.println(classes_list);
+    }
+}

feb9c7eec9855b20553f7fec44fa89863d52b872
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-06 21:45:43 -0300

    using the sort() method of the collections class

--- 10_conteudo_topicos_especiais/02_colecoes_java_collections/01_interface_list/ExemploClasseListaAluno/src/StudentList.java
+++ 10_conteudo_topicos_especiais/02_colecoes_java_collections/01_interface_list/ExemploClasseListaAluno/src/StudentList.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentList {
     public static void main(String[] args) throws Exception {
 
-        List<String> lista = new ArrayList<String>();
-        lista.add("João da Silva");
-        lista.add("Antonio Sousa");
-        lista.add("Lúcia Ferreira");
-        System.out.println(lista);
+        List<String> list_of_strings = new ArrayList<String>();
+        list_of_strings.add("João da Silva");
+        list_of_strings.add("Antonio Sousa");
+        list_of_strings.add("Lúcia Ferreira");
+        System.out.println(list_of_strings);
+        
+        Collections.sort(list_of_strings);
+        System.out.println(list_of_strings);
     }
 }

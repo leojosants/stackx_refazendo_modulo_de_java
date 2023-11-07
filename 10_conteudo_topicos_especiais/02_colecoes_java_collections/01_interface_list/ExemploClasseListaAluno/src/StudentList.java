d5fbc3dd5ff777ba76ae01de9ac185d97db1ce8d
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-06 21:43:05 -0300

    using an implementation of the list interface

---
+++ 10_conteudo_topicos_especiais/02_colecoes_java_collections/01_interface_list/ExemploClasseListaAluno/src/StudentList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentList {
+    public static void main(String[] args) throws Exception {
+
+        List<String> lista = new ArrayList<String>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        System.out.println(lista);
+    }
+}

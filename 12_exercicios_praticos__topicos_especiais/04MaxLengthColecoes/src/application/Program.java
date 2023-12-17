91b9a6bade3214b07900621421c35641bdc4c5e8
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-16 21:13:26 -0300

    Exercise 04

---
+++ 12_exercicios_praticos__topicos_especiais/04MaxLengthColecoes/src/application/Program.java
@@ -0,0 +1,192 @@
+/*package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+
+	private static Set<String> generateInstanceHashSet() {
+		return new HashSet<>();
+	}
+
+	private static int maxLength(Set<String> set) {
+		int count = 0;
+
+		if (set.isEmpty()) {
+			return 0;
+		}
+		else {
+			for (String string : set) {
+				if (string.length() > count) {
+					count = string.length();
+				}
+			}
+		}
+		return count;
+	}
+
+	private static void displayData(Set<String> set) {
+		int count = maxLength(set);
+
+		System.out.println("\n----- Exibindo resultado -----");
+		
+		if(count == 0) {
+			System.out.println("O conjunto está vazio");
+		}
+		else {
+			System.out.printf("A string de maior comprimento no conjunto tem '%d' caracteres", count);			
+		}
+
+	}
+
+	private static void runProgram() {
+		Set<String> programming_languages_set = generateInstanceHashSet();
+		programming_languages_set.add("JavaScript");
+		programming_languages_set.add("Java");
+		programming_languages_set.add("Python");
+		programming_languages_set.add("PHP");
+		displayData(programming_languages_set);
+	}
+
+	public static void main(String[] args) {
+		runProgram();
+	}
+}
+*/
+
+package application;
+
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+
+	private static Scanner generateInstanceScanner() {
+		return new Scanner(System.in);
+	}
+
+	private static int requestTotalStrings(Scanner scanner) throws IllegalAccessException {
+		int total = 0;
+		String total_string = null;
+
+		System.out.print("\nQuantas 'Strings' serão informadas ..? ");
+		total_string = scanner.nextLine().trim();
+		
+		while (total_string.isEmpty()) {
+			System.out.print("-> campo vazio, tente novamente! Quantas 'Strings' serão informadas ..? ");
+			total_string = scanner.nextLine().trim();
+		}
+		
+		total = Integer.parseInt(total_string);
+		
+		if (total < 0) {
+			throw new IllegalAccessException("entrada inválida ..: " + total);
+		}
+	
+		return total;
+	}
+
+	private static void fillSet(Set<String> programming_languages_set, int total_strings, Scanner scanner) {
+		String data = null;
+
+		if (total_strings != 0) {
+			for (int i = 1; i <= total_strings; i++) {
+				System.out.printf("Digite a %dª ..: ", i);
+				data = scanner.nextLine();
+				
+				while (data.isEmpty()) {
+					System.out.printf("-> campo vazio, tente novamente! Digite a %dª ..: ", i);
+					data = scanner.nextLine();
+				}
+
+				programming_languages_set.add(data);
+			}
+		}
+	}
+
+	private static Set<String> generateInstanceHashSet() {
+		return new HashSet<>();
+	}
+
+	private static int maxLength(Set<String> set) {
+		int count = 0;
+
+		if (set.isEmpty()) {
+			return 0;
+		} else {
+			for (String string : set) {
+				if (string.length() > count) {
+					count = string.length();
+				}
+			}
+		}
+		return count;
+	}
+
+	private static void displayData(Set<String> set) {
+		int count = maxLength(set);
+
+		System.out.println("\n----- Exibindo resultado -----");
+
+		if (count == 0) {
+			System.out.printf("O conjunto está vazio ..: %d strings%n", count);
+		} 
+		else {
+			System.out.printf("A string de maior comprimento no conjunto tem '%d' caracteres %n", count);
+		}
+
+	}
+	
+	private static void displayMessageInputMismatchException() {
+		System.out.println("-> erro entrada inválida");
+	}
+
+	private static void displayMessageNumberFormatException() {
+		System.out.println("-> erro entrada inválida");
+	}
+	
+	private static void displayMessageIllegalAccessException(Throwable e) {
+		System.out.printf("-> erro %s%n", e.getMessage());
+	}
+	
+	private static void displayMessageException(Throwable e) {
+		System.out.printf("-> erro %s%n", e);
+	}
+	
+	private static void displayMessageEndOfProgram() {
+		System.out.println("\n----- fim do programa -----");
+	}
+
+	private static void runProgram() {
+		Scanner scanner = generateInstanceScanner();
+
+		try {
+			Set<String> programming_languages_set = generateInstanceHashSet();
+			int total_strings = requestTotalStrings(scanner);
+			fillSet(programming_languages_set, total_strings, scanner);
+			displayData(programming_languages_set);
+		} 
+		catch (InputMismatchException e) {
+			displayMessageInputMismatchException();
+		}
+		catch (NumberFormatException e) {
+			displayMessageNumberFormatException();
+		}
+		catch (IllegalAccessException e) {
+			displayMessageIllegalAccessException(e);
+		} 
+		catch (Exception e) {
+			displayMessageException(e);
+		} 
+		finally {
+			scanner.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		runProgram();
+		displayMessageEndOfProgram();
+	}
+}
